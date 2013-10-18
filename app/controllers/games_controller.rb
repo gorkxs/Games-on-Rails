a3d1fd82144cda0af3f5dbd37afe7a99b63188e5
Gordon Turibamwe <gorkxs@hotmail.com>
2013-10-17 20:15:40 -0400

    New push

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,9 @@
+class GamesController < ApplicationController
+
+	# < -- means that it inherites is: GamesController inherites from ApplicationController
+	# to write something in here
+
+	def index
+		
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GamesOnRails::Application.config.secret_key_base = '0e63404418992403a9bd67b8a2149171036aae56b2de146b9ccf6e8d1eaee2ac840062ec8e358921d8e93c256bec367eabf7d39834a15c6bd212e984ddb4fcdd'
